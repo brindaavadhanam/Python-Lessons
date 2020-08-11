48c94078fe96389ca755d4c81a7fa18461c32554
brind <brindaavadhanam@gmail.com>
2020-08-11 11:10:29 -0700

    initial check in

---
+++ Launchpad HW 1.py
@@ -0,0 +1,6 @@
+'''
+this file will contain code to develop a small web browser application.
+it takes a url as input and displays the contents of the url as the output.
+'''
+
+print ("Inside Launchpad HW 1.")
